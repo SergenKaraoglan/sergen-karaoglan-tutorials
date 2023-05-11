7e9bb1cebd019d99643fbb6a9af3f10f2edf0986
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-05-11 19:09:07 +0100

    webgl init

---
+++ components/webgl.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+
+export default function Main() {
+  return MinimalCanvas();
+}
+
+function MinimalCanvas() {
+  const canvasRef = useRef(null);
+  function main() {
+    const canvas = canvasRef.current;
+    const gl = canvas.getContext("webgl");
+
+    if (gl === null) {
+      alert(
+        "Unable to initialize WebGL. Your browser or machine may not support it."
+      );
+      return;
+    }
+
+    gl.clearColor(1, 0, 0, 1); // red
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  }
+
+  useEffect(() => {
+    main();
+  });
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width="640"
+      height="480"
+      className="m-auto mt-3"
+    ></canvas>
+  );
+}
+
+
+
+// https://webgl2fundamentals.org/
