fc74b4629f1eef503a1c12f3f6fca44e064b1b91
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-05-21 18:39:35 +0100

    Update fractal-tree-p5.jsx

--- src/pages/projects/fractal-tree-p5.jsx
+++ src/pages/projects/fractal-tree-p5.jsx
@@ -7,64 +7,60 @@ const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
 
 const height = 500;
 const width = 500;
-let clear = false;
-export default function FractalTree() {
-  const [depth, setDepth] = useState(5);
+const branchHeight = 10;
+const ratio = 0.75;
+const angleIncrement = 20;
 
+export default function Main() {
+  const [depth, setDepth] = useState(5);
   function handleDepth(e) {
     setDepth(e.target.value);
-    clear = true;
   }
-  
+
+  return (
+    <div className="mx-auto w-fit my-3">
+      <FractalTree depth={depth} />
+      <DepthSlider depth={depth} handleDepth={handleDepth} />
+    </div>
+  );
+}
+
+function FractalTree({ depth }) {
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(width, height).parent(canvasParentRef);
     p5.frameRate(30);
-    p5.background(51);
     p5.stroke(255);
   };
 
   const draw = (p5) => {
-    // draw fractal tree
-    drawBranch(p5, width / 2, height, -90, depth, 20);
-    if (clear) {
-      p5.clear();
-      p5.background(51);
-      p5.stroke(255);
-      clear = false;
-    }
+    p5.background(51);
+    drawBranch(p5, width / 2, height, -90, depth);
   };
 
-  function drawBranch(p5, x1, y1, angle, depth, increment) {
+  function drawBranch(p5, x1, y1, angle, depth) {
     if (depth > 0) {
       let x2 = x1 + p5.cos(p5.radians(angle)) * depth * 10.0;
       let y2 = y1 + p5.sin(p5.radians(angle)) * depth * 10.0;
       p5.line(x1, y1, x2, y2);
-      drawBranch(p5, x2, y2, angle - increment, depth - 1, increment);
-      drawBranch(p5, x2, y2, angle + increment, depth - 1, increment);
+      drawBranch(p5, x2, y2, angle - angleIncrement, depth - 1);
+      drawBranch(p5, x2, y2, angle + angleIncrement, depth - 1);
     }
   }
 
-  return (
-    <>
-      <div className="mx-auto w-fit my-3">
-        <Sketch setup={setup} draw={draw} />
-      </div>
-      <DepthSlider depth={depth} handleDepth={handleDepth} />
-    </>
-  );
+  return <Sketch setup={setup} draw={draw} />;
 }
 
 function DepthSlider({ depth, handleDepth }) {
   return (
-    <div className="flex flex-col">
-      <label className="text-white">Depth</label>
+    <div className="mx-auto w-fit flex justify-center my-3">
+      <label className="sr-only">Depth</label>
       <input
         type="range"
         min="1"
         max="10"
         value={depth}
         onChange={(e) => handleDepth(e)}
-        className="w-40"
+        className="appearance-none bg-blue-500 rounded-lg h-1 thumb-lg-blue-600"
       />
     </div>
   );
