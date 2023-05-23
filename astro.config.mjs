4fba0cc09322b8b852cf0aef8b7bc90b7a8eea5b
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-05-23 22:06:24 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import mdx from "@astrojs/mdx";
 
 import image from "@astrojs/image";
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), image()]
+  integrations: [react(), tailwind(), mdx(), image()],
+  adapter: vercel({
+    analytics: true
+  }),
 });
