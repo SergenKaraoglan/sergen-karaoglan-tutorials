4e2b2548e6a1683ab74450c9fd76b6aeb6f309a6
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-06-06 23:15:42 +0100

    babylonjs test

---
+++ src/components/fractal-tree.svelte
@@ -0,0 +1,61 @@
+<script>
+    import * as BABYLON from 'babylonjs';
+    import { onMount } from 'svelte';
+    import "../styles/global.css"
+    
+    let canvas;
+    onMount(() => {
+        run();
+    });
+
+function run(){
+        const engine = new BABYLON.Engine(canvas);
+        const scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+        const light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0), scene);
+        const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 10, new BABYLON.Vector3(0, 3, 0), scene, );
+        camera.attachControl(canvas, true);
+    
+        const ratio = 0.75;
+        const depth = document.getElementById('depth').value;
+        const angle = 0.5;
+    
+        function genFractalTree(depth, angleZ = 0, angleX = 0, diameter = 0.4, height = 1, x = 0, y = 0, z = 0){
+            if(depth === 0){
+                return;
+            }
+            const branch = BABYLON.MeshBuilder.CreateCylinder("branch", {height: height, diameter: diameter, diameterTop:diameter*ratio}, scene);
+            const branch_mat = new BABYLON.StandardMaterial("branchMat", scene);
+            branch_mat.diffuseColor = new BABYLON.Color3(0, 0, 1);
+            branch.material = branch_mat;
+            
+            const pivot = new BABYLON.TransformNode("pivot");
+            pivot.position = new BABYLON.Vector3(x, y , z);
+            branch.parent = pivot;
+            branch.position = new BABYLON.Vector3(0, height / 2, 0);
+            pivot.rotation = new BABYLON.Vector3(angleX, 0, angleZ);
+    
+            x -= Math.sin(angleZ) * Math.cos(angleX) * height;
+            y += Math.cos(angleZ) * Math.cos(angleX) * height;
+            diameter *= ratio;
+            height *= ratio;
+    
+            genFractalTree(depth - 1, angleZ + angle, angleX, diameter, height, x, y, z);
+            genFractalTree(depth - 1, angleZ - angle, angleX, diameter, height, x, y, z);
+        }
+    
+        genFractalTree(depth);
+        //scene.dispose();
+    
+        engine.runRenderLoop(function(){
+            scene.render();
+        });
+    
+        window.addEventListener('resize', function(){
+            engine.resize();
+        });
+    }
+    </script>
+    
+    <canvas bind:this={canvas} class="mx-auto h-screen-1/2 border"></canvas>
+    <input type="range" min="1" max="10" step="1" value="1" id="depth" on:input={run}>
