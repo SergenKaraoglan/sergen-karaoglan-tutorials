a55a9cab3bd9111fcf9a59d709ee987349c8144b
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-06-18 17:17:11 +0100

    Update Subject-graph.svelte

--- src/lib/Subject-graph.svelte
+++ src/lib/Subject-graph.svelte
@@ -2,35 +2,70 @@
 	import { onMount } from 'svelte';
 	import * as subject_graph from '$lib/scripts/subjects.json';
 
-	let nodes;
 	let svg_canvas;
 	const width = 500;
 	const height = 500;
 
 	const subjects = subject_graph.subjects;
 	const links = subject_graph.links;
-	const rx = 50;
-	const ry = 30;
+	const rx = 60;
+	const ry = 40;
 	onMount(() => {
+	    const nodes = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        nodes.setAttribute('stroke', 'blue')
+        nodes.setAttribute('fill', 'blue')
+        nodes.setAttribute('stroke-width', '1')
 		let cx;
 		let cy;
 		for (const subject of subjects) {
-			cx = Math.random() * (width - rx * 2.5) + rx;
-			cy = Math.random() * (height - ry * 2.5) + ry;
 			// no overlap
-			for (const node of nodes.children) {
-				const x = node.cx.baseVal.value;
-				const y = node.cy.baseVal.value;
-				while (Math.abs(x - cx) < (rx * 4) && Math.abs(y - cy) < (ry * 4)) {
-					cx = Math.random() * (width - rx * 2.5) + rx;
-					cy = Math.random() * (height - ry * 2.5) + ry;
+			while (true) {
+				let finished = true;
+				cx = Math.round(Math.random() * (width - rx * 2) + rx);
+				cy = Math.round(Math.random() * (height - ry * 2) + ry);
+				for (const node of nodes.children) {
+					const x = node.cx.baseVal.value;
+					const y = node.cy.baseVal.value;
+					console.log(cx, cy, x, y);
+
+					if (Math.abs(x - cx) < rx * 2 && Math.abs(y - cy) < ry * 2) {
+						finished = false;
+						break;
+					}
+				}
+				if (finished) {
+					subject.cx = cx;
+					subject.cy = cy;
+					break;
 				}
 			}
+
 			const ellipse = createEllipse(rx, ry, cx, cy);
 			nodes.append(ellipse);
-			// const textNode = createText(subject.name, 250, 250);
-			// svg_canvas.append(textNode);
+			const textNode = createText(subject.name, cx - rx * 0.8, cy);
+			svg_canvas.append(textNode);
 		}
+
+		for (const link of links) {
+			let source;
+			let target;
+			for (const subject of subjects) {
+				if (subject.name === link.source) {
+					source = subject;
+				}
+				if (subject.name === link.target) {
+					target = subject;
+				}
+			}
+			console.log(source, target);
+			const x1 = source.cx;
+			const y1 = source.cy;
+			const x2 = target.cx;
+			const y2 = target.cy;
+			const line = createLine(x1, y1, x2, y2);
+			svg_canvas.append(line);
+		}
+        svg_canvas.append(nodes);
 	});
 
 	function createEllipse(rx, ry, cx, cy) {
@@ -46,16 +81,23 @@
 		const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 		textElement.textContent = text;
 		textElement.setAttribute('fill', 'black');
-		textElement.setAttribute('font-size', '20px');
+		textElement.setAttribute('font-size', '16px');
 		textElement.setAttribute('x', x);
 		textElement.setAttribute('y', y);
 		return textElement;
 	}
+
+	function createLine(x1, y1, x2, y2) {
+		const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+		line.setAttribute('x1', x1);
+		line.setAttribute('x2', x2);
+		line.setAttribute('y1', y1);
+		line.setAttribute('y2', y2);
+		line.setAttribute('stroke-width', 3);
+        line.setAttribute('stroke', 'black');
+        return line;
+	}
 </script>
 
 <svg bind:this={svg_canvas} class="m-auto outline" {width} {height}>
-	<!-- <line x1="10" x2="50" y1="110" y2="150" stroke="black" stroke-width="5"/> -->
-	<g bind:this={nodes} stroke="blue" fill="blue" stroke-width="5">
-		<!-- <ellipse cx="250" cy="250" rx="50" ry="30" /> -->
-	</g>
 </svg>
