c596b08624ce98eb26d2cfd850fcc494538a7708
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-07-03 15:07:00 +0300

    remove title and project button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,13 @@
 			description: 'Interact with and learn to create fractal trees.',
 			demo: '/fractal-tree'
 		},
-		{
-			title: 'HTML Fundementals',
-			image: '',
-			topics: ['HTML', 'DOM', 'Markup'],
-			description: 'Learn the basics of HTML.',
-			demo: '/html-fundementals'
-		}
+		// {
+		// 	title: 'HTML Fundementals',
+		// 	image: '',
+		// 	topics: ['HTML', 'DOM', 'Markup'],
+		// 	description: 'Learn the basics of HTML.',
+		// 	demo: '/html-fundementals'
+		// }
 	];
 </script>
 
@@ -49,60 +49,30 @@
 		</div>
 	</div>
 </div>
-<div id="featured-projects" class="mx-auto max-w-4xl py-20 sm:py-28 lg:py-36">
-	<h2 class="text-center text-3xl font-medium tracking-tight text-black sm:text-4xl py-7">
-		Projects
-	</h2>
+<div id="featured-projects" class="mx-auto max-w-4xl py-20 lg:py-32">
 	<ul class="grid grid-cols-1 gap-6 sm:grid-cols-2">
 		{#each project_list as data}
 			<li class="col-span-2 divide-y divide-gray-200 rounded-lg bg-white shadow">
-				<div class="flex w-full items-center justify-between space-x-6 p-6">
-					<div class="flex-1">
-						<div class="flex items-center space-x-3">
-							<h3 class="text-base font-medium text-gray-900">
-								{data.title}
-							</h3>
+				<a href={data.demo} class="block hover:bg-gray-50">
+					<div class="flex w-full items-center justify-between space-x-6 p-6">
+						<div class="flex-1">
+							<div class="flex items-center space-x-3">
+								<h3 class="text-base font-medium text-gray-900">
+									{data.title}
+								</h3>
+							</div>
+							<p class="mt-1 text-sm text-gray-500">
+								{data.topics.join(', ')}
+							</p>
+							<p class="mt-1 text-sm text-gray-900">
+								{data.description}
+							</p>
 						</div>
-						<p class="mt-1 text-sm text-gray-500">
-							{data.topics.join(', ')}
-						</p>
-						<p class="mt-1 text-sm text-gray-900">
-							{data.description}
-						</p>
-					</div>
-					<div class="h-auto w-1/4 flex-shrink-0">
-						<img src={data.image} alt={data.title} />
-					</div>
-				</div>
-				<div>
-					<div class="-mt-px flex divide-x divide-gray-200">
-						<div class="-ml-px flex w-0 flex-1">
-							<a
-								href={data.demo}
-								class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									class="w-5 h-5"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
-										clip-rule="evenodd"
-									/>
-									<path
-										fill-rule="evenodd"
-										d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								<span class="ml-3">Projects</span>
-							</a>
+						<div class="h-auto w-1/4 flex-shrink-0">
+							<img src={data.image} alt={data.title} />
 						</div>
 					</div>
-				</div>
+				</a>
 			</li>
 		{/each}
 	</ul>
