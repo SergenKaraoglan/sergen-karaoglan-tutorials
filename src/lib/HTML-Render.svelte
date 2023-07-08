bd4a67ff73c1937d7e98d913d084ac77c13e266d
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-07-08 22:14:33 +0100

    refactoring

--- src/lib/HTML-Render.svelte
+++ src/lib/HTML-Render.svelte
@@ -1,97 +1,50 @@
 <script>
+	function createHTML(s) {
+		let html =
+			`<html >
+        <head>
+        <style>
+            * {
+                border: solid;
+            }
+            * {
+                margin: 5px;
+            }
+            </style>
+    </head>
+    <body>` +
+			s +
+			`</body>
+  </html>
+`;
+        return html;
+	}
+
 	let html = '';
-	let html1 = `<html >
-        <head>
-        <style>
-            * {
-                border: solid;
-            }
-            * {
-                margin: 5px;
-            }
-            </style>
-    </head>
-    <body>
-    </body>
-  </html>
-`;
+	let html1 = createHTML('');
+	let html2 = createHTML('<h1 >Pikachu</h1>');
 
-let html2 = `<html >
-        <head>
-        <style>
-            * {
-                border: solid;
-            }
-            * {
-                margin: 5px;
-            }
-            </style>
-    </head>
-    <body>
-        <h1 >Pikachu</h1>
-    </body>
-  </html>
-`;
-
-	let html3 = `<html >
-        <head>
-        <style>
-            * {
-                border: solid;
-            }
-            * {
-                margin: 5px;
-            }
-            </style>
-    </head>
-    <body >
+	let html3 = createHTML(`
         <h1 >Pikachu</h1>
         <p >Pokemon number 25</p> 
-    </body>
-  </html>
-`;
+`);
 
-	let html4 = `<html >
-        <head>
-        <style>
-            * {
-                border: solid;
-            }
-            * {
-                margin: 5px;
-            }
-            </style>
-    </head>
-    <body >
+	let html4 = createHTML(`
         <h1 >Pikachu</h1>
         <p >Pokemon number 25</p> 
         <img src="https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png" >
-    </body>
-  </html>
-`;
+`);
 
-	let html5 = `<html >
-    <head>
-        <style>
-            * {
-                border: solid;
-            }
-            * {
-                margin: 5px;
-            }
-            </style>
-    </head>
+	let html5 = createHTML(`
     <body>
         <div >
         <h1 >Pikachu</h1>
         <p >Pokemon number 25</p> 
     </div>
         <img src="https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png" >
-    </body>
-  </html>
-`;
+`);
 
-let time = 1000
+	let time = 1000;
 	function animate() {
 		html = html1;
 		setTimeout(() => {
@@ -100,19 +53,19 @@ let time = 1000
 
 		setTimeout(() => {
 			html = html3;
-		}, (time * 2));
+		}, time * 2);
 
 		setTimeout(() => {
 			html = html4;
-		}, (time * 3));
+		}, time * 3);
 
-        setTimeout(() => {
-            html = html5;
-        }, (time * 4));
+		setTimeout(() => {
+			html = html5;
+		}, time * 4);
 	}
 
-    animate();
-    setInterval(() => animate(), (time * 5));
+	animate();
+	setInterval(() => animate(), time * 5);
 </script>
 
 <iframe class="mx-auto h-96 w-80 sm:w-96 my-5" srcdoc={html} title="HTML example" />
