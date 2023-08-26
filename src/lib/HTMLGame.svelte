389c1b7e38edfaa87315058919e17f90fbfe7268
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-08-26 15:52:15 +0100

    update drag

--- src/lib/HTMLGame.svelte
+++ src/lib/HTMLGame.svelte
@@ -9,21 +9,31 @@
 
 	function drop(event) {
 		event.preventDefault();
-		let data = event.dataTransfer.getData('text');
-		event.target.innerHTML += elements[data].value;
-		html = `<html><head></head> <body>` + elements[data].value + `</body> </html>`;
-		console.log(html);
+		let id = event.dataTransfer.getData('text');
+		event.target.appendChild(document.getElementById(id));
+        const s = [];
+        for (let i = 0; i < input.children.length; i++) {
+            s.push(elements[input.children[i].id].value);
+        }
+		html = `<html><head>
+            <style>img{height: 128px; width: 128px;}</style>
+            </head> 
+        <body>` + s.join('') + `</body></html>`;
 	}
 </script>
 
 <iframe class="mx-auto h-96 w-80 sm:w-96 my-5 border" srcdoc={html} title="HTML game" />
 
 <div class="flex w-fit mx-auto gap-5 my-7">
-	<div class="border w-64 h-64 mx-auto flex flex-col gap-2">
+	<div
+		class="border w-40 h-36 sm:w-64 sm:h-64 mx-auto flex flex-col gap-2"
+		on:dragover={(event) => event.preventDefault()}
+		on:drop={(event) => drop(event)}
+	>
 		{#each elements as element}
 			<div
 				id={element.id}
-				class="rounded-xl w-full bg-black text-white h-10 text-center"
+				class="rounded-xl w-full bg-black text-white h-10 flex-center"
 				draggable="true"
 				on:dragstart={(event) => event.dataTransfer.setData('text', event.target.id)}
 			>
@@ -33,7 +43,8 @@
 	</div>
 
 	<div
-		class="border w-64 h-64 mx-auto flex flex-col gap-2 overflow-hidden"
+		class="border w-40 h-36 sm:w-64 sm:h-64 mx-auto flex flex-col gap-2 overflow-hidden"
+        id = "input"
 		on:dragover={(event) => event.preventDefault()}
 		on:drop={(event) => drop(event)}
 	/>
